99814deb3715266fbeb92b9599f49478c323760f
pawlitos <100161033+pawlitos@users.noreply.github.com>
2026-05-07 17:52:36 +0200

    feat: wire up HttpClient with fetch and base URL interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
+import { baseUrlInterceptor } from './core/interceptors';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(withFetch(), withInterceptors([baseUrlInterceptor])),
+  ],
 };
